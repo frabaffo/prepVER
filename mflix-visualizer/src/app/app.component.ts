d61a4e1ea728d14e9f44d886c12f8cb43b563279
frabaffo <banfi.francesco@iisgalvanimi.edu.it>
2020-11-18 12:21:25 +0000

    mflix

---
+++ mflix-visualizer/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'mflix-visualizer';
+  results : Object[];
+  obs : Observable<object>;
+  constructor(private http : HttpClient, private sanitizer: DomSanitizer){}
+
+  load10Movies()
+  {
+    this.obs = this.http.get("/list/:num");
+    this.obs.subscribe(this.getData);
+  }
+
+  getData = (data) => {
+    this.results = data;
+  }
+
+  photoURL(urltoSanitize) {
+    console.log(urltoSanitize);
+    return this.sanitizer.bypassSecurityTrustUrl(urltoSanitize);
+}
+}
